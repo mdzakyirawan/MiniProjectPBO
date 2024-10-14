d01b129fd504b87756fcd8aa32103fdde79d97c5
Muhammad Dzaky Irawan <144348757+mdzakyirawan@users.noreply.github.com>
2024-10-14 19:12:42 +0800

    Delete RentalCamera2 directory

--- RentalCamera2/src/main/java/Main/CRUDOperations.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-// Interface untuk operasi CRUD
-public interface CRUDOperations {
-    void addCamera();
-    void displayCameras();
-    void updateCameraStatus(int id);
-    void deleteCamera(int id);
-}
